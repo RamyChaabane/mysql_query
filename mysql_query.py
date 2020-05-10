a99ea13dfa89f1a2ec752ceba7be2a0da2e4d6df
Ramy <ramychaab@gmail.com>
2020-05-10 20:16:43 +0200

    Update mysql_query.py

--- mysql_query.py
+++ mysql_query.py
@@ -2,59 +2,6 @@ import pymysql
 from ansible.module_utils.basic import *
 import re
 
-ANSIBLE_METADATA = {'metadata_version': '1.0',
-                    'status': ['stableinterface'],
-                    'supported_by': 'ATOS'}
-
-DOCUMENTATION = '''
----
-module: nova_cell
-short_description: manage mysql connections in nova_api database
-author: Rami CHAABANE
-description:
-    - Manage mysql connections in nova_api database.
-      If I(split), change connections to point to /etc/puppet/my.cnf.d/account_per_controller.cnf
-      Else: rollback change to point to tripleo file
-options:
-   split:
-     description:
-       - Whether we would like to update connections or not
-     required: true
-   mysql_password:
-     description:
-       - Mysql root password
-     required: true
-   internal_vip:
-     description:
-       - Virtual internal ip (required if split is false)
-     required: false
-   nova_password:
-     description:
-       - Mysql nova password (required if split is false)
-     required: false
-
-requirements:
-    - "python >= 2.7"
-'''
-
-EXAMPLES = '''
-# update mysql connections in nova_api database
-nova_cell:
-  split: true
-  mysql_password: "password_mysql"
-
-# rollback configuration after split
-nova_cell:
-  split: false
-  mysql_password: "password_mysql"
-  internal_vip: "{{ ip_internal_api }}"
-  nova_password: "password_nova"
-'''
-
-RETURN = '''
-  True whenever the update was done (either split is true or false)
-'''
-
 
 class Query:
 
